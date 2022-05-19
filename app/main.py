9a4bc0fb4a8da7fea7cc6db676b7e59fd5aea106
Ken Morrissey <49788044+Psylenceo@users.noreply.github.com>
2022-05-19 15:47:38 -0400

    x1

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
 from machine import Pin, PWM
 
 print()
-print("Hello this is v0.6")
+print("Hello this is v0.6a")
 print()
 
 led_pwm = PWM(Pin(2))
 led_pwm.freq(10)
 led_pwm.duty_u16(32758)
+
+print()
+print("Is it flashing yet?")
+print()
