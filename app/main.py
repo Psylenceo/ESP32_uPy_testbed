97f553025b08903c73a4ecaf00e54e31b291ab55
Ken Morrissey <49788044+Psylenceo@users.noreply.github.com>
2022-05-19 15:42:39 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 from machine import Pin, PWM
 
 print()
-print("Hello this is v0.4")
+print("Hello this is v0.5")
 print()
 
 led_pwm = PWM(Pin(2))
-led_pwm.freq(2)
-led_pwm.duty_u16(32768)
+led_pwm.freq(50)
+led_pwm.duty_u16(65000)
