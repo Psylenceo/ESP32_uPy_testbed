da2452065669ce973a2b841c835aa839a18ae64d
Ken Morrissey <49788044+Psylenceo@users.noreply.github.com>
2022-05-20 01:06:10 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,13 +1,20 @@
-from machine import Pin, PWM
+from machine import Pin, PWM, UART
 
-#print()
-#print("Hello this is v0.6a")
-#print()
+print()
+print("Hello this is v0.6a")
+print()
 
 led_pwm = PWM(Pin(2))
 led_pwm.freq(5)
 led_pwm.duty_u16(32758)
 
-#print()
-#print("Is it flashing yet?")
-#print()
+print()
+print("Is it flashing yet?")
+print()
+
+gps = UART(1, baudrate=115200)
+print(gps.read(72))
+print(gps.read(72))
+print(gps.read(72))
+print(gps.read(72))
+print(gps.read(72))
